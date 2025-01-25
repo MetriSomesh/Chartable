47435f4f13ae5ea96f92da939463b8d03ed7a913
MetriSomesh <someshmetri44@gmail.com>
2025-01-25 18:06:50 +0530

    fix deployment error

--- app/api/getcurrentuser/route.ts
+++ app/api/getcurrentuser/route.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
-import { NextRequest, NextResponse } from "next/server";
+import {  NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   try {
     // Make the request from the server to the Spotify API
     const response = await axios.get(
